9c5504d6a9f736087a33ddde8aa0f77ccbed6a0c
Sameer Devgan <sameedev@amdocs.com>
2020-03-01 19:12:34 +0530

    modified as Microservice code

--- src/main/java/all/TenantController.java
+++ src/main/java/all/TenantController.java
@@ -1,6 +1,10 @@
 package all;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,15 +22,22 @@ public class TenantController {
 	@Autowired
 	RestTemplate template;
 	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
 	@RequestMapping(value="/getTenant/{id}",method=RequestMethod.GET)
 	public Tenant getTenant(@PathVariable(name="id") String id){
 		
 		return service.getTenantById(id);
 	}
+	@RequestMapping(value="/getService/",method=RequestMethod.GET)
+	public List<ServiceInstance> getIn(){
+		return discoveryClient.getInstances("vs");
+	}
 	
 	@RequestMapping(value="/deleteTenant/{id}",method=RequestMethod.DELETE)
 	public void deleteTenant(@PathVariable(name="id") String id){
-		template.delete("http://Vserver-service/tenant/{id}/deleteVserver/", id);
+		template.delete("http://vs/tenant/{id}/deleteVserver/", id);
 		
 		service.deleteTenant(id);;
 	}
--- src/main/java/all/TenantService.java
+++ src/main/java/all/TenantService.java
@@ -6,14 +6,15 @@ import java.util.HashSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-@EnableSwagger2 
-@EnableEurekaClient
+//@EnableSwagger2 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TenantService {
 
@@ -21,7 +22,6 @@ public class TenantService {
 	public static void main(String[] args) {
 		SpringApplication.run(TenantService.class, args);
 		
-		//repo.saveAndFlush(e);
 		
 	}
 
